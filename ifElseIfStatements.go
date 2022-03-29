9c9768fdf04e1228235d3364b3d72a1b22e99ed5
Victor Lin <linvictorw@gmail.com>
2022-03-29 11:50:36 +0800

    Add if else statements

--- ifElseIfStatements.go
+++ ifElseIfStatements.go
@@ -1,6 +1,19 @@
 package main
 
+import "fmt"
+
 func main() {
 	//Insert your code here
 	//Hint if a:= ?? ; condition {  }
+	if num := 17; num%2 == 1 {
+		fmt.Println("Number is odd.")
+	} else if num%2 == 0 {
+		fmt.Println("Number is even.")
+	}
+
+	if num := 17; num < 10 {
+		fmt.Println("Number is single digit.")
+	} else {
+		fmt.Println("Number is multi-digit.")
+	}
 }
